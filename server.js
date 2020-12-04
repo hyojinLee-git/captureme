390759238067e8d014cafc90759b89f0143bd026
rapa1130 <49236828+rapa1130@users.noreply.github.com>
2020-12-05 02:50:32 +0900

    _

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-var http=require("http");
 var fs=require("fs");
-var url=require("url");
 var express=require("express");
-var path=require("path");
 var bodyParser=require('body-parser');
+var session=require('express-session');
 
 var app=express();
 var port=8080;
@@ -24,7 +22,7 @@ app.post('/loginPage',function(request,response){
     //계정확인 성공 -> 세션에 로그인 이력 기록하고 main 페이지로 이동
     //계정확인 실패 -> 로그인 실패 알람 띄우고 다시 로그인 페이지로 이동
 
-    if(id=="12345"&&pw=="11"){
+    if(id=="test"&&pw=="test"){
         //sessionStorage.setItem("id",id); 안댐
         response.writeHead(302,{Location:`/main`});
         response.send();
@@ -41,7 +39,35 @@ app.get('/main',function(request,response){
         response.send(data.toString());
     })
 })
+app.get('/mypage',function(request,response){
+    fs.readFile('public/html/mypage.html',function(err,data){
+        if(err)throw err;
+        response.send(data.toString());
+    })
+})
+app.get('/detail',function(request,response){
+    fs.readFile('public/html/detail.html',function(err,data){
+        if(err)throw err;
+        response.send(data.toString());
+    })
+})
+app.get('/apply',function(request,response){
+    fs.readFile('public/html/apply.html',function(err,data){
+        if(err)throw err;
+        response.send(data.toString());
+    })
+})
+app.post('/applyPage',function(request,response){
+    var body=request.body;
+    var id=body.id;
+    var pw=body.password;
+    var name=body.name;
+    var phone=body.phoneNumber;
+
+    response.writeHead(302,{Location:`/`});
+    response.send();
+})
 
 app.listen(port,function(){
-    console.log("뿅");
+    console.log("server is running at "+port+" port");
 })
