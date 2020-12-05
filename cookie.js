0e5cf48eea9f58eee73245e122b0f6512c0d79d5
rapa1130 <49236828+rapa1130@users.noreply.github.com>
2020-12-06 02:04:59 +0900

    _

--- cookie.js
+++
@@ -1,8 +0,0 @@
-var http=require('http');
-var cookie=require('cookie');
-
-
-http.createServer(function(request,response){
-    response.writeHead(200,{"Set-Cookie":[`newCookie=this; Max-Age=${60*60}`]});
-    response.end("cookie");
-}).listen(8080);
--- server.js
+++ server.js
@@ -2,28 +2,53 @@ var fs=require("fs");
 var express=require("express");
 var bodyParser=require('body-parser');
 var session=require('express-session');
+var FileStore=require('session-file-store')(session);
+var mypageTemplate=require('./lib/mypage');
+const { request } = require("express");
 
 var app=express();
 var port=8080;
 
+function checkApplyUser(request){
+    var fileList=fs.readdirSync('userInfo','utf8');
+    for(var i=0;i<fileList.length&&fileList[i]==request.body.id+'.json';i++){
+        var data=fs.readFileSync(`userInfo/${fileList[i]}`,'utf8');
+        var userInfo=JSON.parse(data);
+        if(userInfo.account.password==request.body.password){
+            return true;
+        }
+    }
+    return false;
+}
+function athentication(request){
+    return request.session.is_logined;
+}
+
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(session({
+    secret:"aa",
+    resave:false,
+    saveUninitialized:true,
+    store:new FileStore()
+}))
 
 app.get('/',function(request,response){
     fs.readFile('public/html/login.html',function(err,data){
         if(err)throw err;
         response.send(data.toString());
+        
     })
 })
 
 app.post('/loginPage',function(request,response){
-    var id=request.body.id;
-    var pw=request.body.password;
     //계정확인 성공 -> 세션에 로그인 이력 기록하고 main 페이지로 이동
     //계정확인 실패 -> 로그인 실패 알람 띄우고 다시 로그인 페이지로 이동
-
-    if(id=="test"&&pw=="test"){
-        //sessionStorage.setItem("id",id); 안댐
+    //sql로 수정
+    if(checkApplyUser(request)){
+        request.session.is_logined=true;
+        request.session.userId=request.body.id;
         response.writeHead(302,{Location:`/main`});
         response.send();
     }
@@ -31,21 +56,28 @@ app.post('/loginPage',function(request,response){
         response.writeHead(302,{Location:`/`});
         response.send();
     }
+    
 })
 
 app.get('/main',function(request,response){
+    if(!athentication(request)){return false;}
     fs.readFile('public/html/main.html',function(err,data){
         if(err)throw err;
         response.send(data.toString());
     })
 })
+
 app.get('/mypage',function(request,response){
-    fs.readFile('public/html/mypage.html',function(err,data){
-        if(err)throw err;
-        response.send(data.toString());
-    })
+    if(!athentication(request)){return false;}
+    response.send(mypageTemplate.html(request));
 })
+app.get('/mypage/:pageId',function(request,response){
+    if(!athentication(request)){return false;}
+    response.send(mypageTemplate.html(request));
+})
+
 app.get('/detail',function(request,response){
+    if(!athentication(request)){return false;}
     fs.readFile('public/html/detail.html',function(err,data){
         if(err)throw err;
         response.send(data.toString());
@@ -58,12 +90,16 @@ app.get('/apply',function(request,response){
     })
 })
 app.post('/applyPage',function(request,response){
-    var body=request.body;
-    var id=body.id;
-    var pw=body.password;
-    var name=body.name;
-    var phone=body.phoneNumber;
-
+    var body={account:request.body};
+    //sql로 수정
+    fs.writeFile(`./userInfo/${body.account.id}.json`,JSON.stringify(body),'utf8',function(err){
+        if(err)throw err;
+        response.writeHead(302,{Location:`/`});
+        response.send();
+    })
+})
+app.get('/logoutPage',function(request,response){
+    request.session.destroy();
     response.writeHead(302,{Location:`/`});
     response.send();
 })
